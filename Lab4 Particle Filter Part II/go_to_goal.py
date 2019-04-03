18288c7c3b0cf6b2b46f7aab52c8d8d36f9a2028
Ting Qiu <tqiu43@gatech.edu>
2019-04-03 12:24:00 -0400

    lab 4 done

--- Lab4 Particle Filter Part II/go_to_goal.py
+++ Lab4 Particle Filter Part II/go_to_goal.py
@@ -1,3 +1,5 @@
+# George Jeno, Ting Qiu 
+
 ## If you run into an "[NSApplication _setup] unrecognized selector" problem on macOS,
 ## try uncommenting the following snippet
 
@@ -9,6 +11,7 @@
 
 from skimage import color
 import cozmo
+from cozmo.util import Pose, degrees, speed_mmps, distance_inches, distance_mm
 import numpy as np
 from numpy.linalg import inv
 import threading
@@ -16,6 +19,7 @@ import time
 import sys
 import asyncio
 from PIL import Image
+import math
 
 from markers import detect, annotator
 
@@ -75,7 +79,7 @@ def compute_odometry(curr_pose, cvt_inch=True):
         last_pose.rotation.angle_z.degrees
     curr_x, curr_y, curr_h = curr_pose.position.x, curr_pose.position.y, \
         curr_pose.rotation.angle_z.degrees
-    
+
     dx, dy = rotate_point(curr_x-last_x, curr_y-last_y, -last_h)
     if cvt_inch:
         dx, dy = dx / grid.scale, dy / grid.scale
@@ -85,7 +89,7 @@ def compute_odometry(curr_pose, cvt_inch=True):
 
 async def marker_processing(robot, camera_settings, show_diagnostic_image=False):
     '''
-    Obtain the visible markers from the current frame from Cozmo's camera. 
+    Obtain the visible markers from the current frame from Cozmo's camera.
     Since this is an async function, it must be called using await, for example:
 
         markers, camera_image = await marker_processing(robot, camera_settings, show_diagnostic_image=False)
@@ -95,7 +99,7 @@ async def marker_processing(robot, camera_settings, show_diagnostic_image=False)
         - camera_settings: 3x3 matrix representing the camera calibration settings
         - show_diagnostic_image: if True, shows what the marker detector sees after processing
     Returns:
-        - a list of detected markers, each being a 3-tuple (rx, ry, rh) 
+        - a list of detected markers, each being a 3-tuple (rx, ry, rh)
           (as expected by the particle filter's measurement update)
         - a PIL Image of what Cozmo's camera sees with marker annotations
     '''
@@ -108,7 +112,7 @@ async def marker_processing(robot, camera_settings, show_diagnostic_image=False)
     # Convert the image to grayscale
     image = np.array(image_event.image)
     image = color.rgb2gray(image)
-    
+
     # Detect the markers
     markers, diag = detect.detect_markers(image, camera_settings, include_diagnostics=True)
 
@@ -150,13 +154,109 @@ async def run(robot: cozmo.robot.Robot):
     ], dtype=np.float)
 
     ###################
+    at_goal = False
+    picked_up = False
 
-    # YOUR CODE HERE
+    count = 0
+
+    """                 This requires a file to be in the same directory, functionality was demoed               """
+    #angry_img = cozmo.oled_face.convert_image_to_screen_data(Image.open("angery.jpg").resize(cozmo.oled_face.dimensions(), Image.NEAREST))
+
+    while (True):
+        #robot.enable_stop_on_cliff(False)
+        await robot.say_text("").wait_for_completed()
+        if (robot.is_picked_up):
+            if (not picked_up):
+                at_goal = False
+                pf.particles = Particle.create_random(PARTICLE_COUNT, grid)
+                #await robot.display_oled_face_image(angry_img, 3000.0).wait_for_completed()
+                await robot.set_head_angle(cozmo.util.degrees(0)).wait_for_completed()
+                picked_up = True
+            continue
+
+        if (at_goal):
+            continue
+
+        if (not flag_odom_init):
+            last_pose = robot.pose
+            flag_odom_init = True
+            continue
+
+        picked_up = False
+
+        curr_odom = compute_odometry(robot.pose)
+        last_pose = robot.pose
+
+        markers, camera_image = await marker_processing(robot, camera_settings, show_diagnostic_image=False)
+
+        m_x, m_y, m_h, m_confident = pf.update(curr_odom, markers)
+
+        #robot.pose = cozmo.util.Pose(m_x,m_y,0,angle_z=cozmo.util.Angle(degrees=m_h))
+
+        gui.show_particles(pf.particles)
+        gui.show_mean(0,0,0)
+        gui.show_camera_image(camera_image)
+        gui.updated.set()
+
+        count += 1
+
+        if (m_confident):
+            #robot.go_to_pose(cozmo.util.Pose(goal[0], goal[1], 0, angle_z=cozmo.util.Angle(degrees=goal[2])))
+            robot.stop_all_motors()
+            dx = goal[0] - m_x
+            dy = goal[1] - m_y
+            angle = math.atan2(dy,dx)
+            distance = grid_distance(dx,dy,0,0)
+
+            print(diff_heading_deg(math.degrees(angle), m_h))
+
+            await robot.turn_in_place(degrees(diff_heading_deg(math.degrees(angle), m_h))).wait_for_completed()
+            robot.wait_for_all_actions_completed()
+            await robot.drive_straight(distance_inches(distance), speed_mmps(140)).wait_for_completed()
+            robot.wait_for_all_actions_completed()
+            await robot.turn_in_place(degrees(-1*diff_heading_deg(-5,math.degrees(angle)))).wait_for_completed()
+
+
+            at_goal = True
+
+            print("Done")
+        else:
+            """
+            if (len(markers) > 0):
+                await robot.turn_in_place(degrees(5)).wait_for_completed()
+            else:
+                await robot.turn_in_place(degrees(20)).wait_for_completed()
+
+            if (count % 20 == 0):
+                robot.stop_all_motors()
+                await robot.drive_straight(distance_inches(3), speed_mmps(100)).wait_for_completed()
+            """
+            if (count % 5 == 0):
+                await robot.set_head_angle(cozmo.util.degrees(0)).wait_for_completed()
+
+            if (count > 30 and len(markers) > 0):
+                robot.stop_all_motors()
+                await robot.drive_straight(distance_inches(markers[0][0] * 0.5), speed_mmps(100)).wait_for_completed()
+                await robot.turn_in_place(degrees(100)).wait_for_completed()
+                await robot.set_head_angle(cozmo.util.degrees(0)).wait_for_completed()
+                count -= 30
+
+            """
+            if (len(markers) > 0):
+                robot.drive_wheel_motors(-10,10,-1000,1000)
+            else:
+                robot.drive_wheel_motors(-20,20,-1000,1000)
+            """
+            if (len(markers) > 0):
+                await robot.turn_in_place(degrees(10)).wait_for_completed()
+            else:
+                await robot.turn_in_place(degrees(20)).wait_for_completed()
+
 
     ###################
 
 class CozmoThread(threading.Thread):
-    
+
     def __init__(self):
         threading.Thread.__init__(self, daemon=False)
 
@@ -175,4 +275,3 @@ if __name__ == '__main__':
     gui.show_particles(pf.particles)
     gui.show_mean(0, 0, 0)
     gui.start()
-
